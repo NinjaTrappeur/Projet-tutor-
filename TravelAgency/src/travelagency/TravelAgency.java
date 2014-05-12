3268cb99d99b75f48a36501856ad0d80650f5837
Félix Baylac-Jacqué <baylac.felix@gmail.com>
2014-05-12 10:57:40 +0200

    Travel Agency squeletton.

---
+++ TravelAgency/src/travelagency/TravelAgency.java
@@ -0,0 +1,13 @@
+
+
+package travelagency;
+
+public class TravelAgency {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
