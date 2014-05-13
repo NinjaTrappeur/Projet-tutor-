846173ba6133bbfb6515828c25a4df73588b5a14
Félix Baylac-Jacqué <baylac.felix@gmail.com>
2014-05-13 21:51:01 +0200

    Adding message traduction to fractal application.

--- Fractal/TravelAgency/src/travelagency/WsLink.java
+++ Fractal/TravelAgency/src/travelagency/WsLink.java
@@ -1,7 +1,12 @@
 package travelagency;
 
+import jade.core.AID;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import message.IOffer;
+import message.Offer;
+import message.OfferPack;
+import message.OfferRequest;
 
 
 @WebService
@@ -17,10 +22,24 @@ public class WsLink {
     }
     
     public webservice.message.OfferPack getOffers(@WebParam(name = "offerRequest")webservice.message.OfferRequest offerRequest){
-        return new webservice.message.OfferPack(new webservice.message.Offer(0,"",""));
+        OfferRequest offerRequestM = 
+                new OfferRequest(offerRequest.getHighestPrice(),
+                        offerRequest.getLowestPrice(), 
+                        offerRequest.getClientName(),
+                        offerRequest.getDepartureDate(),
+                        offerRequest.getReturnDate(),
+                        offerRequest.getPlaceName(), 
+                        offerRequest.getTimeGuard());
+        OfferPack pack = _client.getOffers(offerRequestM);
+        IOffer offer = pack.lowestPrice();
+        return new webservice.message.OfferPack(new webservice.message.Offer(offer.price(),
+                offer.companyName(),"Composant"));
     }
     
     public webservice.message.ConfirmationLetter makeReservation(@WebParam(name = "offer")webservice.message.Offer offer) {
+        //TODO, traduire AID.
+        Offer offerM = new Offer(offer.getPrice(), offer.getName(), new AID());
+        _client.makeReservation(offerM);
         return new webservice.message.ConfirmationLetter();
     }
 }
