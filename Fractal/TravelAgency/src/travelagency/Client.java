aef9eff973f1259cbd5accbd6601bcf2eaf866bd
Félix Baylac-Jacqué <baylac.felix@gmail.com>
2014-05-13 17:06:34 +0200

    Adding fake client behaviour.

--- Fractal/TravelAgency/src/travelagency/Client.java
+++ Fractal/TravelAgency/src/travelagency/Client.java
@@ -1,10 +1,16 @@
 package travelagency;
 
-import travelagency.interfaces.IVacationOfferBrowser;
-import travelagency.interfaces.IVacationReservationManager;
+import java.util.Date;
+import message.IConfirmationLetter;
+import message.IOfferPack;
+import message.Offer;
+import message.OfferPack;
+import message.OfferRequest;
 import org.objectweb.fractal.fraclet.annotations.Component;
 import org.objectweb.fractal.fraclet.annotations.Interface;
 import org.objectweb.fractal.fraclet.annotations.Requires;
+import travelagency.interfaces.IVacationOfferBrowser;
+import travelagency.interfaces.IVacationReservationManager;
 
 
 
@@ -19,6 +25,11 @@ public class Client implements Runnable{
     
     @Override
     public void run() {
-        System.out.println("Architecture OK.");
+        System.out.println("Émmission d'une requête d'offres.");
+        IOfferPack offer = getOfferInterface.getProposals(new OfferRequest(100, 200, "toto", new Date(), new Date(), "", 0));
+        System.out.println("Résultat: " + offer.toString());
+        System.out.print("Émmision d'une requête de validation.");
+        IConfirmationLetter letter = makeReservationInterface.reserveOffer(new Offer(0,""));
+        System.out.println("Résultat: " + letter.toString());
     }
 }
